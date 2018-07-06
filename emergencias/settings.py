1873f3da24d4bdca2e2c230101a1ac32ca143f5c
Jose Miguel Subelza <jose_miguels@hotmail.com>
2018-07-06 18:22:04 -0400

    Change Static Files

--- emergencias/settings.py
+++ emergencias/settings.py
@@ -132,8 +132,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
